7eb9f28fa0d223cb4055116d68779601d968904c
RodJunio <devjuniorodrigues@gmail.com>
2023-06-30 06:42:21 -0300

    ajuste de porta

--- api-desafio21dias/Servico/DbContexto.cs
+++ api-desafio21dias/Servico/DbContexto.cs
@@ -6,7 +6,6 @@ namespace api_desafio21dias.Servicos
     public class DbContexto : DbContext
     {
         public DbContexto(DbContextOptions<DbContexto> options) : base(options) { }
-        public DbSet<Aluno> Alunos { get; set; }
-        public DbSet<Fornecedor> Fornecedores { get; set; }
+        public DbSet<Aluno> Alunos { get; set; }        
     }
 }
